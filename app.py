576905eee1e77972d05b82889e96f55f95e1b3ef
abdullahCode511 <abdullah.alotaibi12020@gmail.com>
2024-07-31 14:59:29 +0300

    added multiple streams

--- app.py
+++ app.py
@@ -5,16 +5,20 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-vid = cv2.VideoCapture("rtsp://admin:Mstech12@10.5.50.227:554/Streaming/Channels/101")
+# Dictionary to hold multiple video streams
+streams = {
+    "stream1": "rtsp://admin:huem92000@10.5.50.247:554/Streaming/Channels/101",
+    "stream2": "rtsp://admin:huem92000@10.5.50.191:554/Streaming/Channels/101"  # Example additional stream
+}
 
-vid.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'H264'))
-
-def gen_frames():
+def gen_frames(stream_url):
+    vid = cv2.VideoCapture(stream_url)
+    vid.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'H264'))
     while True:
         success, frame = vid.read()
         if not success:
             vid.release()
-            vid.open("rtsp://admin:Mstech12@192.168.0.153:554/Streaming/Channels/101")
+            vid.open(stream_url)
             continue
         else:
             frame = cv2.resize(frame, (640, 480))
@@ -27,13 +31,19 @@ def gen_frames():
 def index():
     return render_template('index.html')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed/<stream_id>')
+def video_feed(stream_id):
+    if stream_id in streams:
+        return Response(gen_frames(streams[stream_id]), mimetype='multipart/x-mixed-replace; boundary=frame')
+    else:
+        return "Stream not found", 404
 
-@app.route('/embed')
-def embed():
-    return render_template('embed.html')
+@app.route('/embed/<stream_id>')
+def embed(stream_id):
+    if stream_id in streams:
+        return render_template('embed.html', stream_id=stream_id)
+    else:
+        return "Stream not found", 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
